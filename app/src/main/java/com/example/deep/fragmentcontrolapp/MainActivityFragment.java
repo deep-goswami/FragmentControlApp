53cfce338a149dfb766878cbb5d30bc6e5b88893
deep-goswami <eng.deepgoswamideep4444@gmail.com>
2016-12-26 12:43:13 +0600

    test commit

---
+++ app/src/main/java/com/example/deep/fragmentcontrolapp/MainActivityFragment.java
@@ -0,0 +1,39 @@
+package com.example.deep.fragmentcontrolapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.view.View;
+
+import layout.FragmentA;
+import layout.FragmentB;
+
+public class MainActivityFragment extends AppCompatActivity {
+
+
+    FragmentA fra= new FragmentA();
+    FragmentB frb= new FragmentB();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_fragment);
+    }
+
+    public void change(View v){
+        FragmentManager fm=getFragmentManager();
+        FragmentTransaction ft=fm.beginTransaction();
+        ft.replace(R.id.fragment,fra,"Fragment1");
+        ft.commit();
+    }
+
+    public void change2(View v){
+        FragmentManager fm=getFragmentManager();
+        FragmentTransaction ft=fm.beginTransaction();
+        ft.replace(R.id.fragment,frb,"Fragment2");
+        ft.commit();
+    }
+
+}
